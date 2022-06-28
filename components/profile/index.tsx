ee19284d3eab3d5e99380b86f38432f28b55b997
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-06-27 23:20:29 -0300

    feat: profile image

--- components/profile/index.tsx
+++ components/profile/index.tsx
@@ -1,9 +1,19 @@
 import styles from "./profile.module.css";
+import Image from "next/image";
 
 export default function Profile() {
   return (
     <header className={styles.profile}>
-      <div>Perfil</div>
+      <Image
+        className={styles.photo}
+        src="/pedroluis.png"
+        alt="me"
+        width={124}
+        height={124}
+      ></Image>
+
+      <div>Pedro Luís Brilhadori</div>
+      <div>Front End Developer at AGRO TI | Co-Organizer RP.js</div>
     </header>
   );
 }
