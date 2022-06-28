b8c8cf67d9aa54b56f92631a0dd9d10ebe1a5f67
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-06-27 22:02:47 -0300

    feat: create profile component

---
+++ components/profile/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./profile.module.css";
+
+export default function Profile() {
+  return (
+    <header className={styles.profile}>
+      <div>Perfil</div>
+    </header>
+  );
+}
