e0d964d6be0e9f964789d3488ece742d32ee6ef4
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-08-01 18:54:41 -0300

    chore: change project name

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 export { default as SignInComponent } from "./login/login";
-export { default as AdmScreen } from "./adm/admScreen";
+export { default as AdmScreen } from "./adm/adm-screen";
 export { default as CardComponent } from "./card/card";
 export { default as ProfileComponent } from "./profile/profile";
 export { default as InfoComponent } from "./info/info";
